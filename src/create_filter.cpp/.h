86d1f67553e19ab3fa6e45ccd48edc21c4a6fb8f
yaeltsu <yael.tsu@gmail.com>
2024-01-18 15:59:35 +0200

    branch

--- src/create_filter.cpp/.h
+++ src/create_filter.cpp/.h
@@ -7,12 +7,15 @@ using namespace std;
 
 class create_bloom_filter : public ICommand{
     public:
+    vector<bool> bitArray;
+    
+
     void execute(input) {
         int array_size = input[1]
         int hash1 = input[2]
         int hash2 = input[3]
 
         bitArray.resize(array_size, false); // Initialize the bit array with false values
-        
+
     }
 }
