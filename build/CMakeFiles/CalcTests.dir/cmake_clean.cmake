0df1a4a81b47cee3ec709b660f312dbc73eb0575
shahar9991 <shahar9991@gmail.com>
2024-01-18 15:52:26 +0200

    string tests

---
+++ build/CMakeFiles/CalcTests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalcTests.dir/tests/calc_test.cpp.o"
+  "CMakeFiles/CalcTests.dir/tests/calc_test.cpp.o.d"
+  "CMakeFiles/CalcTests.dir/tests/test_main.cpp.o"
+  "CMakeFiles/CalcTests.dir/tests/test_main.cpp.o.d"
+  "CalcTests"
+  "CalcTests.pdb"
+  "CalcTests[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalcTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/shaha/Desktop/part1-progect1
+# Build directory: /mnt/c/Users/shaha/Desktop/part1-progect1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/mnt/c/Users/shaha/Desktop/part1-progect1/build/CalcTests[1]_include.cmake")
