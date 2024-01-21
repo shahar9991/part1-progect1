52d6e1933b024b057e5404ed56ee391c0cdfa50c
yaeltsu <yael.tsu@gmail.com>
2024-01-21 18:42:57 +0200

    IExecutable

---
+++ src/AddURL.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+#include <string>
+
+class AddURL : public IExecutable {
+private:
+    std::vector<bool> bitArray;
+    std::function<size_t(const std::string&)> hashFunction1;
+    std::function<size_t(const std::string&)> hashFunction2;
+
+public:
+    void execute(){}
+};
