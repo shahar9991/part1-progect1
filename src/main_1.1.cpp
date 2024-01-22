6826d1697153f4fded3718b3f2f42cef322472a3
shahar9991 <shahar9991@gmail.com>
2024-01-22 12:41:53 +0200

    1.1 update

--- src/main_1.1.cpp
+++ src/main_1.1.cpp
@@ -1,21 +1,17 @@
-// mainofinput.cpp
-#include "InputHandler.h"
 #include "ArgsHandler.h"
 #include "URLsHandler.h"
 #include "BloomFilter.h"
 #include <iostream>
 #include <functional>
-#include <vector>  // Add this include for vector
+#include <vector>
 
 int main() {
     int size;
     std::vector<int> args;
-    ArgsHandler argsHandler;
-    URLsHandler urlsHandler;
-    BloomFilter bloomFilter;
 
-    // Use InputHandler to get user input
-    ArgsHandler::readSizeAndArgs(size, args);
+    ArgsHandler argsHandler;
+    argsHandler.readSizeAndArgs(size, args);
+    
 
     // Decide which hash functions to use based on user input
     bool useHash1 = std::find(args.begin(), args.end(), 1) != args.end();
@@ -23,7 +19,7 @@ int main() {
 
     // Define hash functions based on user input
     std::function<size_t(const std::string&)> hashFunction1 = std::hash<std::string>();
-    std::function<size_t(const std::string&)> hashFunction2;
+    std::function<size_t(const std::string&)> hashFunction2 = nullptr;
 
     if (useHash1) {
         hashFunction1 = std::hash<std::string>();
@@ -31,16 +27,17 @@ int main() {
         hashFunction1 = [](const std::string&) -> size_t { return static_cast<size_t>(0); };
     }
 
-     if (useHash2) {
+    if (useHash2) {
         // Create the second hash function using std::hash twice
         hashFunction2 = [](const std::string& s) -> size_t {
             size_t hashValue1 = std::hash<std::string>()(s);
             size_t hashValue2 = std::hash<std::string>()(std::to_string(hashValue1));
-            return hashValue2;
+            return hashValue2 + hashValue2; // Return twice the result
         };
     } else {
         hashFunction2 = [](const std::string&) -> size_t { return static_cast<size_t>(0); };
     }
+
     // Initialize BloomFilter using user inputs
     BloomFilter bloomFilter(size, hashFunction1, hashFunction2);
 
@@ -56,10 +53,9 @@ int main() {
     // Continuously read URLs from the user until an exit condition is met
     while (true) {
         std::vector<std::string> urls;  // Declare the vector to store URLs
-        URLsHandler::readURLs(size, args, urls, bloomFilter);
+        URLsHandler urlsHandler;
+        urlsHandler.readURLs(size, args, urls, bloomFilter);
+    }
 
     return 0;
 }
-
-}
-
