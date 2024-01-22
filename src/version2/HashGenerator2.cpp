b4397cef6530b0d8cb2709b9a0da6cd3151bf213
shahar9991 <shahar9991@gmail.com>
2024-01-23 00:10:23 +0200

    delete function not needed in hash generator

--- src/version2/HashGenerator2.cpp
+++ src/version2/HashGenerator2.cpp
@@ -32,21 +32,6 @@ const std::unordered_map<int, HashGenerator2::HashFunction>& HashGenerator2::get
     return hashFunctionMap;
 }
 
-// Function to iterate over true hash functions and print information
-void HashGenerator2::iterateOverTrueHashFunctions() const {
-    std::cout << "True Hash Functions:\n";
-    for (const auto& entry : hashFunctionMap) {
-        int hashFunctionKey = entry.first;
-        HashFunction hashFunction = entry.second;
-
-        // Use hashFunction as needed (for example, call it with an input)
-        size_t result = hashFunction("example_input");
-
-        // Print information about the true hash function
-        std::cout << "Hash Function " << hashFunctionKey << ": Result=" << result << "\n";
-    }
-}
-
 // Function to get the true keys set
 const std::unordered_set<size_t>& HashGenerator2::getTrueKeysSet() const {
     return trueKeysSet;
