b3b085ad634dd36627235c86e0c8639b4a54dcd4
yaeltsu <yael.tsu@gmail.com>
2024-01-22 11:03:24 +0200

    implement input handler in flow

--- src/Flow.cpp
+++ src/Flow.cpp
@@ -1,18 +1,19 @@
 #include "InputHandler.h"
+#include <iostream>
 
 //this class represents the flow of the program- getting input from the user and calling the right functions.
 class Flow{
     private:
-        IExecutable* commands[]; //list of commands to execute.
+        map<string, IExecutable*> commands; //map of commands to execute.
+        InputHandler* inputHandler;
 
     public:
-        Flow(IExecutable* commands[]) : commands(commands) {}
+        Flow(InputHandler* inputHandler, map<string, IExecutable*> commands;) : inputHandler(inputHandler), commands(commands) {}
         void run(){ //run the program
             while (true) {
-                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 //InputHandler- get the input and make sure it is OK
                 //according to the input- understand which command needs to be executed
-                task = InputHandler();
+                std::string task = InputHandler->handle_input();
                 try {
                     commands[task]->execute(); //try to execute the required task from the commands list.
                     }
--- src/InputHandler.h
+++ src/InputHandler.h
@@ -11,4 +11,5 @@ public:
     static void processURLs(int size, const std::vector<int>& args, const std::vector<std::string>& urls, BloomFilter& bloomFilter);
     static void readSizeAndArgs(int& size, std::vector<int>& args);
     static void readURLs(int size, const std::vector<int>& args, std::vector<std::string>& urls, BloomFilter& bloomFilter);
+    string handle_input();
 };
